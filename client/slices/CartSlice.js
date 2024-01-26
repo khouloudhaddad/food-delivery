fb841c558f6f931e978b84a9bd3a36746e0b1f1a
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-01-26 23:31:46 +0100

    Fix error

--- client/slices/CartSlice.js
+++ client/slices/CartSlice.js
@@ -32,7 +32,7 @@ export const { addToCart, removeFromCart, emptyCart } = CartSlice.actions;
 
 export const selectCartItems = (state) => state.cart.items;
 
-export const selectCartItemsById = (state, id) => state.basket.items.filter((item) => item.id == id);
+export const selectCartItemsById = (state, id) => state.cart.items.filter((item) => item.id == id);
 
 export const selectCartTotal = state => state.cart.items.reduce((total, item) => total = total + item.price, 0)
 
